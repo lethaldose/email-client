cdbf577029ec3d846d735073352e200c5a4ac55c
Atul Verma <atulverma82@gmail.com>
2018-02-04 23:25:40 +1100

    Adding home page and tabs

---
+++ src/components/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home-container">
+    <div class="md-display-1 heading">Email Client</div>
+    <div>
+      <md-tabs md-sync-route>
+        <md-tab id="tab-home" md-label="Home" to="/components/tabs/home"></md-tab>
+        <md-tab id="tab-dashboard" md-label="Email" to="/components/tabs/email"></md-tab>
+      </md-tabs>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Email Client'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .heading {
+    font-weight: normal;
+    text-align: left;
+    border-bottom: 1px solid #e6e6e6;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(VueMaterial)
 
 /* eslint-disable no-new */
 new Vue({
